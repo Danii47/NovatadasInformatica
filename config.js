a5db400a249c218dc94fd277978ae9ed83981f17
Dani47 <87781904+Danii47@users.noreply.github.com>
2024-08-17 17:17:41 +0200

    feat: add mongoose_connect

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 export const {
   PORT = 3000,
   SALT_ROUNDS = 10,
-  SECRET_JWT_KEY = 'this-is-an-awesome-secret-key-please-change-it-in-production'
+  SECRET_JWT_KEY = 'this-is-an-awesome-secret-key-please-change-it-in-production',
+  MONGOOSE_CONNECT
 } = process.env
