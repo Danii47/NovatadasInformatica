6104d0123d0103c027c11346a380375b244db115
Dani47 <87781904+Danii47@users.noreply.github.com>
2024-09-09 16:47:31 +0200

    feat: add server url config string

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 export const {
   PORT = 3000,
+  SERVER_URL = 'http://localhost',
   SALT_ROUNDS = 10,
   SECRET_JWT_KEY = 'this-is-an-awesome-secret-key-please-change-it-in-production',
   MONGOOSE_CONNECT
