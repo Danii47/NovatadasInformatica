78e48e2668b5592223d50f4b22f81cfc7020eff3
Dani47 <87781904+Danii47@users.noreply.github.com>
2024-09-09 14:36:53 +0200

    feat: custom errors classes

---
+++ errors.js
@@ -0,0 +1,90 @@
+export class ValidationError extends Error {
+  constructor (message) {
+    super(message)
+    this.name = 'ValidationError'
+  }
+}
+
+export class UserAlreadyExistsError extends Error {
+  constructor (message) {
+    super(message)
+    this.name = 'UserAlreadyExistsError'
+  }
+}
+
+export class UserNotFoundError extends Error {
+  constructor (message) {
+    super(message)
+    this.name = 'UserNotFoundError'
+  }
+}
+
+export class UserAlreadyAdministratorError extends Error {
+  constructor (message) {
+    super(message)
+    this.name = 'UserAlreadyAdministratorError'
+  }
+}
+
+export class InvalidCredentialsError extends Error {
+  constructor (message) {
+    super(message)
+    this.name = 'InvalidCredentialsError'
+  }
+}
+
+export class ForbiddenError extends Error {
+  constructor (message) {
+    super(message)
+    this.name = 'ForbiddenError'
+  }
+}
+
+export class UnauthorizedError extends Error {
+  constructor (message) {
+    super(message)
+    this.name = 'UnauthorizedError'
+  }
+}
+
+export class ChallengeNotFoundError extends Error {
+  constructor (message) {
+    super(message)
+    this.name = 'ChallengeNotFoundError'
+  }
+}
+
+export class ChallengeAlreadyCompletedError extends Error {
+  constructor (message) {
+    super(message)
+    this.name = 'ChallengeAlreadyCompletedError'
+  }
+}
+
+export class ChallengeAlreadyPendingError extends Error {
+  constructor (message) {
+    super(message)
+    this.name = 'ChallengeAlreadyPendingError'
+  }
+}
+
+export class ChallengeAlreadyAcceptedError extends Error {
+  constructor (message) {
+    super(message)
+    this.name = 'ChallengeAlreadyAcceptedError'
+  }
+}
+
+export class ChallengeNotRequestedError extends Error {
+  constructor (message) {
+    super(message)
+    this.name = 'ChallengeNotRequestedError'
+  }
+}
+
+export class ConnectionError extends Error {
+  constructor (message) {
+    super(message)
+    this.name = 'ConnectionError'
+  }
+}
