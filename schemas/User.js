b78d51e084362b6816a1fc18dbcb088b4db18e50
Dani47 <87781904+Danii47@users.noreply.github.com>
2024-09-10 23:00:50 +0200

    refact: User schema

--- schemas/User.js
+++ schemas/User.js
@@ -1,11 +1,17 @@
 import { Schema, model } from 'mongoose'
 
+const ExtraPointsSchema = new Schema({
+  name: { type: String, required: true },
+  points: { type: Number, required: true }
+})
+
 const User = new Schema({
   dni: { type: String, required: true },
   name: { type: String, required: true },
   password: { type: String, required: true },
   pendingChallenges: { type: Array, required: true, default: [] },
   challenges: { type: Array, required: true, default: [] },
+  extraPoints: [ExtraPointsSchema],
   points: { type: Number, required: true, default: 0 },
   isAdmin: { type: Boolean, required: true, default: false },
   isSuperAdmin: { type: Boolean, required: true, default: false }
